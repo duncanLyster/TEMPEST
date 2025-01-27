e9369e709f2c94241e5762ef582c4f1af01c97dc
Duncan Lyster <duncan.lyster@physics.ox.ac.uk>
2025-01-27 14:49:34 +0000

    Changed temperatures storage to just save previous timestep at all layers, and previous day for all timesteps at surface level. ~45x reduction in RAM usage.

--- src/model/simulation.py
+++ src/model/simulation.py
@@ -41,21 +41,23 @@ class Simulation:
 
 class ThermalData:
     def __init__(self, n_facets, timesteps_per_day, n_layers, max_days, calculate_energy_terms):
-        self.temperatures = np.zeros((n_facets, timesteps_per_day * max_days, n_layers), dtype=np.float64)
+        # Surface temperatures for one day only
+        self.temperatures = np.zeros((n_facets, timesteps_per_day), dtype=np.float64)
+        # Two columns for current and previous timestep subsurface temperatures
+        self.layer_temperatures = np.zeros((n_facets, 2, n_layers), dtype=np.float64)
         self.insolation = np.zeros((n_facets, timesteps_per_day), dtype=np.float64)
         self.visible_facets = [np.array([], dtype=np.int64) for _ in range(n_facets)]
         self.secondary_radiation_view_factors = [np.array([], dtype=np.float64) for _ in range(n_facets)]
         self.thermal_view_factors = [np.array([], dtype=np.float64) for _ in range(n_facets)]
 
-        self.calculate_energy_terms = calculate_energy_terms
-
         if calculate_energy_terms:
-            self.insolation_energy = np.zeros((n_facets, timesteps_per_day * max_days))
-            self.re_emitted_energy = np.zeros((n_facets, timesteps_per_day * max_days))
-            self.surface_energy_change = np.zeros((n_facets, timesteps_per_day * max_days))
-            self.conducted_energy = np.zeros((n_facets, timesteps_per_day * max_days))
-            self.unphysical_energy_loss = np.zeros((n_facets, timesteps_per_day * max_days))
-            
+            # Energy terms for one day only
+            self.insolation_energy = np.zeros((n_facets, timesteps_per_day))
+            self.re_emitted_energy = np.zeros((n_facets, timesteps_per_day))
+            self.surface_energy_change = np.zeros((n_facets, timesteps_per_day))
+            self.conducted_energy = np.zeros((n_facets, timesteps_per_day))
+            self.unphysical_energy_loss = np.zeros((n_facets, timesteps_per_day))
+
     def set_visible_facets(self, visible_facets):
         self.visible_facets = [np.array(facets, dtype=np.int64) for facets in visible_facets]
 
