c9d79e55285948555bb11caccc1d4504f9505fdb
Duncan Lyster <duncan.lyster@physics.ox.ac.uk>
2025-07-18 10:14:47 +0100

    Updated solvers init

--- src/model/solvers/__init__.py
+++ src/model/solvers/__init__.py
@@ -1,7 +1,5 @@
 from .factory import TemperatureSolverFactory
 from .tempest_standard import TempestStandardSolver
-from .thermprojrs import ThermprojrsSolver
 
 # Register available solvers
-TemperatureSolverFactory.register("tempest_standard", TempestStandardSolver)
-TemperatureSolverFactory.register("thermprojrs", ThermprojrsSolver) 
+TemperatureSolverFactory.register("tempest_standard", TempestStandardSolver) 
